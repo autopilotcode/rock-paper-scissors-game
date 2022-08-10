ba0d69a2c9e9b25ba8411961f57b4cb472877a2f
Andrii Falieiev <falieievandrii@gmail.com>
2022-08-10 15:09:09 +0300

    user choice added

--- app.js
+++ app.js
@@ -13,6 +13,31 @@ const startGameBtn = document.getElementById("start-game-btn");
 // console.dir(start);
 //startGameBtn.addEventListener('click', start);        //indirect execution
 
-startGameBtn.addEventListener("click", function start() {
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  // const selection = prompt("Rock, Paper or Scissirs?", "");
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, "").toUpperCase(); //toUpperCase method
+  if (
+      selection !== ROCK && 
+      selection !== PAPER && 
+      selection !== SCISSORS
+      ) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener("click", function() {
+  if (gameIsRunning) {       //if true - stop the game
+    return;
+  }
   console.log("The game is starting...");
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
