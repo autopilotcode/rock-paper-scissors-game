ae0ce9bece58a7e0ffed6f6203ed8bab3d95e3d0
Andrii Falieiev <falieievandrii@gmail.com>
2022-08-13 17:45:52 +0300

    current

--- app.js
+++ app.js
@@ -92,7 +92,6 @@ startGameBtn.addEventListener("click", () => {
     } else {
       winner = getWinner(computerChoice); //we know that plyaerChoice is undefined
     }
-
   //         if playerChoise is truthy use playerChoise otherwise use DEFAULT_USER_CHOICE
   // let message = `'You picked' ${playerChoise ? playerChoise : DEFAULT_USER_CHOICE}, 'computer picked' ${computerChoice}, 'therefore you ' `;
   let message = `'You picked' ${playerChoise || DEFAULT_USER_CHOICE}, 'computer picked' ${computerChoice}, 'therefore you ' `;
@@ -108,36 +107,49 @@ startGameBtn.addEventListener("click", () => {
   gameIsRunning = false; //now we can tap to bottom and start a new game
 
   //my own additional output
-  console.log(
-    "computerChoice is:",
-    computerChoice,
-    " - Calculation method is: <0.34=ROCK, <0.67=PAPER, otherwise=SCISSORS"
-  );
-  console.log("playerSelection is:", playerChoise);
-  console.log("winner is:", winner);
+      console.log(
+        "computerChoice is:",
+        computerChoice,
+        " - Calculation method is: <0.34=ROCK, <0.67=PAPER, otherwise=SCISSORS"
+      );
+      console.log("playerSelection is:", playerChoise);
+      console.log("winner is:", winner);
 });
 
 
 
+
+
+
+//-----------------------------------------------
 //not related to the game (rest operator practice)
 
 let a, b;
-const sumUp = (a, b, ...numbers) => {
+const sumUp = (resultHandler, ...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+
   let sum = 0;
     for (const num of numbers) {
-      sum += num;
+      sum += validateNumber(num);
     } 
-    return sum;
+    resultHandler(sum);
 };
 
 const substractUp = function() {
   let sum = 0;
-  for (const num of arguments) {  //don't use this
+  for (const num of arguments) {  //don't use this (arguments - build in operator)
     sum -= num;
   }
   return sum;
 };
 
-console.log(sumUp(1, 5, 10, -3, 6, 10));
-console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+const showResult = (result) => {
+  alert('The result after adding all numbers is: ' + result);
+}
+
+//here sumUo will execute showResult
+sumUp(showResult, 5, 'qwqe', -3, 6, 1);
+sumUp(showResult, 5, 10, -3, 6, 10, 25, 88);
 console.log(substractUp(1, 10, 15, 20));
